10aafea5a0ca563a0811cdbab7c0f45ee0c90d5c
Christian <christian.omlin@siemens.com>
2020-08-18 14:32:27 +0200

    feat(components): register new visual components

--- src/index.js
+++ src/index.js
@@ -7,6 +7,9 @@ import GenericRolls from './components/Generic/GenericRolls/GenericRolls';
 import GenericTimeseries from './components/Generic/GenericTimeseries/GenericTimeseries';
 import GenericValue from './components/Generic/GenericValue/GenericValue';
 import PieChart from './components/PieChart/PieChart';
+import EnergyProvider from './components/RollspecificGoals/EnergyProvider/EnergyProvider';
+import Industry from './components/RollspecificGoals/Industry/Industry';
+import Planer from './components/RollspecificGoals/Planer/Planer';
 import RollspecificGoals from './components/RollspecificGoals/RollspecificGoals';
 
 export const ExampleComponent = ({ text }) => {
@@ -16,10 +19,13 @@ export const ExampleComponent = ({ text }) => {
 export const visualComponentDict = {
   donut_chart: DonutChart,
   donut_chart_2: DonutChart2,
+  energy_provider: EnergyProvider,
   generic_rolls: GenericRolls,
   generic_timeseries: GenericTimeseries,
   generic_value: GenericValue,
+  industry: Industry,
   pie_chart: PieChart,
+  planer: Planer,
   rollspecific_goals: RollspecificGoals,
   carbon_budget: CarbonBudget
 }
